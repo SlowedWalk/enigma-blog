4afb9b1e3196203a7afd4ec57f6edd7af2536d03
SlowedWalk <idetoratojo1@gmail.com>
2023-09-02 16:48:09 +0100

    feature: added roles check

--- src/api/auth/requireAuth.jsx
+++ src/api/auth/requireAuth.jsx
@@ -1,13 +1,17 @@
 import { useSelector } from 'react-redux'
-import { selectCurrentToken } from './authSlice';
+import { selectCurrentToken, selectCurrentUser } from './authSlice';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
-export const RequireAuth = () => {
+export const RequireAuth = ({
+  allowedRoles // add allowedRoles prop to the RequireAuth to check if the user has the required role
+ }) => {
   const token = useSelector(selectCurrentToken);
+  const user = useSelector(selectCurrentUser);
   const location = useLocation();
 
   return (
     token
+        // && user.roles?.find(role => allowedRoles?.includes(role))  // check if the user has the required role
         ? <Outlet />
         : <Navigate to='/login' state={{ from: location }} replace={true} />
         // replace={true} prevents the login page from being added to the history stack
